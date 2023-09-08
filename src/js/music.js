79a7385507960afaa0f99a975e24389b00017d95
vlcty <88147999+devVelocity@users.noreply.github.com>
2023-09-08 06:42:37 +0100

    custom toast

--- src/js/music.js
+++ src/js/music.js
@@ -1,10 +1,36 @@
 const { Howl, Howler } = require('howler');
 window.$ = window.jQuery = require('jquery');
+const Store = require('electron-store');
+const store = new Store();
+
 
 var playing = false;
-
 var sound;
 
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function triggerToast(type,msg){
+    if(type == "s"){
+        $('#toasty .success').toggleClass('messageActive')
+        $('#toasty .success .message').text(msg)
+        $('#toasty').toggleClass('toastyActive')
+        await sleep(5000);
+        $('#toasty').toggleClass('toastyActive')
+        $('#toasty .success').toggleClass('messageActive')
+    }else if(type=="e"){
+        $('#toasty .error').toggleClass('messageActive')
+        $('#toasty .error .message').text(msg)
+        $('#toasty').toggleClass('toastyActive')
+        await sleep(5000);
+        $('#toasty').toggleClass('toastyActive')
+        $('#toasty .error').toggleClass('messageActive')
+    }
+}
+
+ 
 function loadSound(src){
     sound = new Howl({
         src: './mp3/test_sound_scotrail.mp3',
@@ -43,8 +69,28 @@ $('#stopClick').on("click", function(){
 })
 
 $('#filePicker').on("change", function(e){
-    console.log("hi")
-    console.log(e.target.files[0])
+    console.log(e.target.files[1].path)  
+    var newObj = []
+    var failedImports = 0;
+    for(const file in e.target.files){
+        var path = e.target.files[file].path
+        console.log(path)
+        if(path != undefined){
+            if(path.includes(".mp3") || path.includes(".wav")){
+                newObj.push({"path":path})
+            }else{
+                failedImports++
+            }
+        }
+    }   
+
+    if(failedImports > 0){
+        triggerToast("e",`${failedImports} files failed to import.`)
+    }
+    store.set('music_files', newObj)
+    console.log(store.get())
+
+    $('fileForm').val('');
 })
 
 loadSound('./mp3/test_sound_scotrail.mp3')
